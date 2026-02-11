97e7ef2a0b2b4f44a4d6c492972ee5299dbca47f
Dalinte <kollekcionerssd1@gmail.com>
2026-02-11 19:48:05 +0700

    add posthog and event

--- components/ExploreButton.tsx
+++ components/ExploreButton.tsx
@@ -1,10 +1,17 @@
 'use client'
 
 import Image from "next/image";
+import posthog from 'posthog-js'
 
 const ExploreButton = () => {
+  function handlePurchase() {
+    console.log('Sending event to PostHog...')
+    posthog.capture('explore_button_click')
+    console.log('Event sent!')
+  }
+
   return (
-    <button type='button' id="explore-button" className="mt-7 mx-auto" onClick={() => console.log('111')}>
+    <button type='button' id="explore-button" className="mt-7 mx-auto" onClick={handlePurchase}>
       <a href="#events">
         Explore Events
         <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}/>
