d9793b3c2ff06f94a4282f859cc71d061da5be87
Dalinte <kollekcionerssd1@gmail.com>
2026-02-16 18:23:55 +0700

    add booking event, add simular events, fix image in eventcard, format event page

--- components/event/EventCard.tsx
+++ components/event/EventCard.tsx
@@ -9,7 +9,7 @@ const EventCard = ({ title, image, slug, location, date, time }: Event) => {
         <Image src={image} alt={title} width={410} height={300} className="poster" />
       </div>
       <div className="flex flex-row gap-2">
-        <Image src={'icons/pin.svg'} alt={'location'} width={14} height={14} />
+        <Image src={'/icons/pin.svg'} alt={'location'} width={14} height={14} />
         <p>{location}</p>
       </div>
       <p className="title">{title}</p>
